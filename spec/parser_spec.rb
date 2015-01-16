ba8aeea4f014a91acb744444dc3ebd4ed0816f03
James Carlson <jxxcarlson@mac.com>
2015-01-16 09:51:49 -0500

    - Saying 'rspec spec/parser_spec.rb' in the root of this repo   now works even if you are not me.

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,10 +2,10 @@ require 'rspec'
 require_relative '../lib/reader'
 require_relative '../lib/parser'
 
-ROOT = '/Users/carlson/dev/git/latex2asciidoctor/text/'
+ROOT = File.expand_path File.dirname(__FILE__).gsub('spec', 'text')
 
 def path file
-  ROOT+file
+  ROOT+"/"+file
 end
 
 def compress(str)
@@ -14,73 +14,17 @@ end
 
 describe Parser do
 
-=begin
 
   it 'can get tokens from a file' do
 
-    text = IO.read(path('0.tex'))
-    puts text.cyan
+    text = IO.read(path('1.tex'))
     parser = Parser.new(text)
-    parser.reader.display
-
-    puts parser.get_token.value
-    puts parser.get_token.value
-    puts parser.get_token.value
-    puts parser.get_token.value
-    puts parser.get_token.value
-    puts parser.get_token.value
-
-
-  end
-
-
-
-  it 'can get tokens from a file' do
-
-    text = IO.read(path('1  .tex'))
-    puts text.cyan
-    parser = Parser.new(text)
-    parser.reader.display
+    # parser.reader.display
     expect(parser.get_token.value).to eq '%% This is a test file'
-    expect(parser.get_token.value).to eq :blank_line
-    expect(parser.get_token.value).to eq '\begin{document}'
-    expect(parser.get_token.value).to eq :blank_line
-    expect(parser.get_token.value).to eq '\newcommand{\DT}{{\mathbb'
-    expect(parser.get_token.value).to eq  'S}}'
-
-  end
-
-  it 'can display element of the stack' do
-
-    parser = Parser.new('')
-
-    str = "foo bar"
-    parser.display_element str
-
-    symbol = :foo
-    parser.display_element symbol
-
-    token = Token.new(:yuuk, 'more')
-    parser.display_element token
-
-    node = parser.new_node('hoho')
-    parser.display_element node
-
-    node2 = parser.new_node([1, 2, 3])
-    parser.display_element node2
-
-    array = [str, symbol, token, node, node2]
-    parser.display_element array
-
-
 
   end
 
 
-
-
-
-=end
 
 
   it 'can recognize 0.tex as a valid file' do
