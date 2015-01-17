e3434fe3d0f7d9f0f2e5de8104ea1b1c7a2ebe0b
James Carlson <jxxcarlson@mac.com>
2015-01-17 14:04:03 -0500

    New: class Node < Tree::TreeNode

--- lib/node.rb
+++ lib/node.rb
@@ -1,49 +1,7 @@
+require 'tree'
+
+class Node < Tree::TreeNode
 
 
-class Tree::TreeNode
-
-
-  def basic_render
-    content = self.content
-    content[:value]
-  end
-
-  def render_environment
-    content = self.content
-    content[:value].map{ |token| token.value }.string_join
-  end
-
-
-  def render
-    content = self.content
-    type = content[:type]
-    case type
-      when :header, :text, :comment, :macro, :comment, :end_document
-        self.basic_render
-      when :body
-        '\begin{document}' << "\n" << "\n"
-      when :inline_math
-        "#{content[:value]}" << "\n"
-      when :display_math
-        "\\[#{content[:value]}\\]" << "\n"
-      when :environment
-        self.render_environment
-      else
-        puts "FUNNY NODE, #{node.name}".magenta
-        display_node node
-        puts "======== end funny =========".magenta
-        "\n" << node.name
-    end
-  end
-
-  def render_tree
-    tip = self
-    text = ""
-    while tip do
-      text += tip.render
-      tip = tip.first_child
-    end
-    text
-  end
 
 end
